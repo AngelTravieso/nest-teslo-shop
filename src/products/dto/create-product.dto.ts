2acdb7735e33094d017a39a42aab9fd5f4bc7956
AngelTravieso <angeltraviesoc@gmail.com>
2023-07-22 11:43:24 -0400

    Create Product DTO"

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,35 @@
-export class CreateProductDto {}
+import { IsArray,IsIn, IsInt, IsNumber, IsOptional, IsPositive, IsString, MinLength } from "class-validator";
+
+export class CreateProductDto {
+
+    @IsString()
+    @MinLength(1)
+    title: string;
+
+    @IsNumber() // => acepta doubles
+    @IsPositive()
+    @IsOptional()
+    price?: number;    
+
+    @IsString()
+    @IsOptional()
+    description?: string;
+
+    @IsString()
+    @IsOptional()
+    slug?: string;
+
+    @IsInt() // => enteros
+    @IsPositive()
+    @IsOptional()
+    stock?: number;
+
+    @IsString({ each: true }) // => cada item del array debe ser un string
+    @IsArray()
+    size: string[];
+
+    @IsIn(['men', 'women', 'kid', 'unisex']) // => deben venir solo estos valores
+    @IsString()
+    gender: string;
+
+}
