b72be54ca3d145a2b46a60d50c65e6f14890b0b8
AngelTravieso <angeltraviesoc@gmail.com>
2023-07-22 11:29:36 -0400

    Entidad sin relaciones

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -12,4 +12,36 @@ export class Product {
     }) // => @Column('tipo_column') OJO: No todos los tipos de datos son aceptados por postgres
     title: string;
 
+    @Column('numeric', {
+        default: 0, // => valor default del campo
+    })
+    price: number;
+
+    @Column({
+        type: 'text', // => también se puede definir el tipo del campo así
+        nullable: true, // => puede aceptar nulos
+    })
+    description: string;
+
+    @Column('text', {
+        unique: true,
+    })
+    slug: string;
+
+    @Column('int', {
+        default: 0,
+    })
+    stock: number;
+
+    @Column('text', {
+        array: true,
+    })
+    size: string[];
+
+    @Column('text')
+    gender: string;
+
+    // tags
+    // images
+
 }
