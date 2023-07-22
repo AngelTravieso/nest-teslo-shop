81d459eb586037306361f4dd5f7fd7e04d9ab9c8
AngelTravieso <angeltraviesoc@gmail.com>
2023-07-22 10:37:08 -0400

    Conectar NestJS con PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    // Establecer variables de entorno
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      // si hay algun cambio en las entidades las sincroniza, usualmente no se usa en producción
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
